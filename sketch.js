9546990f7fa381e2b84f26f884ae7eaa799f965d
Ridhima <pantridhima2323@gmail.com>
2025-11-15 04:13:55 +0530

    Trigger GitHub Pages deployment

--- sketch.js
+++ sketch.js
@@ -1,70 +1,70 @@
+let points = [];
+let k = 3;
+
 class Point {
   constructor() {
-    this.x = Math.random() * width;
-    this.y = Math.random() * height;
-    this.class = floor(Math.random() * 2);
+    this.x = random(width);
+    this.y = random(height);
+    this.class = floor(random(2));
   }
 
   display() {
     stroke(0);
     strokeWeight(2);
 
-    if (this.class == 1) {
-      fill(200, 120, 230);
-    } else {
-      fill(0, 200, 200);
-    }
-
-    ellipse(this.x, this.y, 20, 20);
+    fill(this.class === 1 ? "rgb(200,120,230)" : "rgb(0,200,200)");
+    circle(this.x, this.y, 20);
   }
 }
 
-var points = [];
-
 function setup() {
-  createCanvas(300, 300);
+  let canvas = createCanvas(400, 400);
+  canvas.parent("canvasWrapper");
 
-  for (var i = 0; i < 20; i++) {
-    points.push(new Point());
-  }
+  generatePoints();
+
+  // UI bindings
+  document.getElementById("regenBtn").addEventListener("click", generatePoints);
+  document.getElementById("kSlider").addEventListener("input", (event) => {
+    k = event.target.value;
+    document.getElementById("kValue").innerText = k;
+  });
 }
 
 function draw() {
-  background(240);
+  background(245);
 
-  for (var i = 0; i < points.length; i++) {
-    points[i].display();
+  for (let p of points) {
+    p.display();
   }
+
   classifyMouse();
+}
+
+// randomize dataset
+function generatePoints() {
+  points = [];
+  for (let i = 0; i < 25; i++) {
+    points.push(new Point());
+  }
 }
 
 function classifyMouse() {
   let distances = [];
 
-  for (var i = 0; i < points.length; i++) {
-    let distance = dist(points[i].x, points[i].y, mouseX, mouseY);
-    distances.push([distance, points[i].class]);
+  for (let p of points) {
+    let d = dist(p.x, p.y, mouseX, mouseY);
+    distances.push([d, p.class]);
   }
-  
-  distances.sort((a, b) => a[0] - b[0]);
-  
-  let numZero = 0;
-  let numOne = 0;
 
-  for (var i = 0; i < 3; i++) {
-    if (distances[i][1] == 1) {
-      numOne++;
-    } else {
-      numZero++;
-    }
+  distances.sort((a,b) => a[0] - b[0]);
+
+  let count0 = 0, count1 = 0;
+  for (let i = 0; i < k; i++) {
+    distances[i][1] === 1 ? count1++ : count0++;
   }
 
   noStroke();
-  if (numOne > numZero) {
-    fill(200, 120, 230);
-  } else {
-    fill(0, 200, 200);
-  }
-
-  ellipse(mouseX, mouseY, 10, 10);
+  fill(count1 > count0 ? "rgb(200,120,230)" : "rgb(0,200,200)");
+  circle(mouseX, mouseY, 12);
 }
